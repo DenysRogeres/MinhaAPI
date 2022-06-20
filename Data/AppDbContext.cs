0d79f6638c684e5643b294f14edca26c354852d3
Denys Rógeres <dennysrogeres@gmail.com>
2022-06-20 00:22:52 -0300

    adicionando sqlite

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaAPI.Models;
+
+namespace MinhaAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=app.db; Cache=Shared");
+    }
+}
