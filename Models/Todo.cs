b7fd49e65aacb6bd9e26a200cdf351ee7e1e81ea
Denys Rógeres <dennysrogeres@gmail.com>
2022-06-20 00:09:48 -0300

    criado models

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace MinhaAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
