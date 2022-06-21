c21f4c4c983f69a18cfcfcc0e0a646e5be6671ac
Denys Rógeres <dennysrogeres@gmail.com>
2022-06-21 00:47:08 -0300

    finalizando CRUD

--- Migrations/20220621031412_CreateDatabase.Designer.cs
+++ Migrations/20220621031412_CreateDatabase.Designer.cs
@@ -10,7 +10,7 @@ using MinhaAPI.Data;
 namespace MinhaAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20220620151341_CreateDatabase")]
+    [Migration("20220621031412_CreateDatabase")]
     partial class CreateDatabase
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
